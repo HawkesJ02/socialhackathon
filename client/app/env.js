e5676a51a7c854fa0a32cd30c7f339a5f3d68076
Jaden Hawkes <jadenhawkes@gmail.com>
2023-02-17 12:08:51 -0700

    ENV

--- client/app/env.js
+++ client/app/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'hawkes-k.us.auth0.com'
+export const audience = 'https://cwhk-api.com '
+export const clientId = 'E16Sh8x2ye2tLTvdiMHccd6Awzk5lodG'
